baecacbbfb799c0461a0d32c8541a4531f550e17
BuildTools <unconfigured@null.spigotmc.org>
2022-02-17 10:05:13 +0100

    bug fixes and ant reset on scorpion collision

--- HelpProcedures.rkt
+++ HelpProcedures.rkt
@@ -1,17 +1,3 @@
-;; Executes a function on each element of the given data-structure
-(define (map-object function lst)
-  (map function lst))
-
+;; Executes a function on each element of the given abstract data-structure
 (define (for-each-object function lst)
   (for-each function lst))
-
-(define (remove-from-list element lst)
-  (let ((search-list (reverse lst)))
-    (let search-and-remove ((current (car search-list))
-                            (remaining (cdr search-list))
-                            (result '()))
-      (cond
-        ((and (null? remaining) (eq? current element)) result)
-        ((null? remaining) (cons current result))
-        ((eq? current element) (append result remaining))
-        (else (search-and-remove (car remaining) (cdr remaining) (cons current result)))))))
