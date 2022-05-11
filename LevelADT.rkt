b8fe74c23ec348573bde9525c18c9aa6fe10b62c
Vic De Hondt <vic.dehondt@gmail.com>
2022-05-11 22:45:59 +0200

    Added bomb with weak walls support

--- LevelADT.rkt
+++ LevelADT.rkt
@@ -5,6 +5,8 @@
         (ant (make-movingobject initial-ant-pos 'right 'ant))
         (inventory '((empty) (empty) (empty)))
         (update-score? #f)
+        (bomb-animation? #f)
+        (weak-wall-direction 0)
         (lives 0)
         (remove-live? #f)
         (add-live? #f)
@@ -16,38 +18,33 @@
         (eggs '())
         (keys '())
         (doors '())
+        (weak-walls '())
+        (bombs '())
         (shield-shrooms '())
         (food '())
         (reset-level? #f)
         (done? #f)
-        (back-up (make-vector 5 0)))
+        (back-up (make-vector 5 '())))
 
 
     (define (duplicate!)
       (let ((new-level (make-level level-number initial-ant-pos top-border bottom-border end-point)))
-        (new-level 'add-walls (vector-ref back-up wall-place))
-        (new-level 'add-eggs (vector-ref back-up egg-place))
-        (new-level 'add-scorpions (vector-ref back-up scorpion-place))
-        (new-level 'add-puzzleobjects (vector-ref back-up puzzle-object-place))
-        (new-level 'add-powerups (vector-ref back-up power-up-place))
+        (if (not (null? (vector-ref back-up wall-place))) (new-level 'add-walls (vector-ref back-up wall-place)))
+        (if (not (null? (vector-ref back-up egg-place))) (new-level 'add-eggs (vector-ref back-up egg-place)))
+        (if (not (null? (vector-ref back-up scorpion-place))) (new-level 'add-scorpions (vector-ref back-up scorpion-place)))
+        (if (not (null? (vector-ref back-up puzzle-object-place))) (new-level 'add-puzzleobjects (vector-ref back-up puzzle-object-place)))
+        (if (not (null? (vector-ref back-up power-up-place))) (new-level 'add-powerups (vector-ref back-up power-up-place)))
         new-level))
 
     
     (define (initial-ant-pos! position-object)
       (set! initial-ant-pos position-object))
 
-    (define (check-deactivate-speed-up! delta-time)
-      (if (> speed-up-time speed-up-interval)
-          (begin
-            (set! speed-up? #f)
-            (set! speed-up-time 0))))
-
-    (define (speed-up delta-time)
-      (set! speed-up-time (+ speed-up-time delta-time))
-      (if (and (member (random 1 128) '(5 53 27 83 63 101)) (> speed-up-time speed-up-interval))
-          (begin
-            (set! speed-up? #t)
-            (set! speed-up-time 0))))
+    (define (speed-up . value)
+      (if (null? value)
+          speed-up?
+          (set! speed-up? (car value))))
+    
 
     ;;
     ;; ADD PROCEDURES
@@ -129,6 +126,8 @@
       (cond
         ((eq? kind 'key) (set! keys (cons (make-puzzleobject position-object kind) keys)))
         ((eq? kind 'door) (set! doors (cons (make-puzzleobject position-object kind) doors)))
+        ((eq? kind 'weak-wall) (set! weak-walls (cons (make-puzzleobject position-object kind) weak-walls)))
+        ((eq? kind 'bomb) (set! bombs (cons (make-puzzleobject position-object kind) bombs)))
         (else (error "[ERROR in LevelADT add-puzzleobject] Wrong kind!"))))
 
 
@@ -157,18 +156,39 @@
           ((and (list? (member #t (door-collision-list moving-object direction))) (eq? (caar inventory) 'empty)) #f)
           (else #t)))
 
+      (define (check-for-bomb)
+        (cond
+          ((and (list? (member #t (weak-wall-collision-list moving-object direction))) (eq? (caadr inventory) 'bomb)) (set! weak-wall-direction direction)
+           (bomb-animation #t) #f)
+          ((and (list? (member #t (weak-wall-collision-list moving-object direction))) (eq? (caadr inventory) 'empty)) #f)
+          (else #t)))
+
       (cond
         ((eq? kind 'wall) (not (list? (member #t (wall-collision-list moving-object direction)))))
         ((eq? kind 'door) (if (eq? (moving-object 'kind) 'ant) (check-for-key)
                               (not (list? (member #t (door-collision-list moving-object direction))))))
+        ((eq? kind 'weak-wall) (if (eq? (moving-object 'kind) 'ant) (check-for-bomb)
+                                   (not (list? (member #t (weak-wall-collision-list moving-object direction))))))
+        ((eq? kind 'bomb) (not (list? (member #t (bomb-collision-list moving-object)))))
         ((eq? kind 'egg) (not (list? (member #t (egg-collision-list moving-object)))))
         ((eq? kind 'key) (not (list? (member #t (key-collision-list moving-object)))))
         ((eq? kind 'shield-shroom) (not (list? (member #t (shield-shroom-collision-list moving-object)))))
         ((eq? kind 'food) (not (list? (member #t (food-collision-list moving-object)))))
         (else (error "[ERROR in LevelADT can-move?] Wrong kind!"))))
 
+    (define (bomb-animation . values)
+      (if (null? values)
+          bomb-animation?
+          (begin
+            (set! bomb-animation? (car values))
+            (if (not (car values))
+                (remove! 'weak-wall weak-wall-direction)))))
+
 
     ;; Collision-lists: gives list back with #t (collision) and #f (no obstruction)
+
+    (define (collision-list moving-object lst)
+      (map (lambda (object) (collision? moving-object object)) lst))
     
     (define (egg-collision-list moving-object)
       (map (lambda (egg-object) (collision? moving-object egg-object)) eggs))
@@ -187,6 +207,12 @@
 
     (define (key-collision-list moving-object)
       (map (lambda (key-object) (collision? moving-object key-object)) keys))
+
+    (define (weak-wall-collision-list moving-object direction)
+      (map (lambda (weak-wall-object) (upcomming-collision? moving-object (weak-wall-object 'position) direction)) weak-walls))
+
+    (define (bomb-collision-list moving-object)
+      (map (lambda (bomb-object) (collision? moving-object bomb-object)) bombs))
 
 
     ;; Collision checkers
@@ -222,15 +248,18 @@
 
       (cond
         ((eq? kind 'door) (remove-from-inventory! 'key) (set! doors (search-and-remove! doors (next-position ant (car direction)))))
+        ((eq? kind 'weak-wall) (remove-from-inventory! 'bomb) (set! weak-walls (search-and-remove! weak-walls (next-position ant (car direction)))))
         ((eq? kind 'egg) (set! eggs (search-and-remove! eggs (ant 'position))))
         ((eq? kind 'shield-shroom) (set! shield-shrooms (search-and-remove! shield-shrooms (ant 'position))))
         ((eq? kind 'food) (set! food (search-and-remove! food (ant 'position))))
         (else (error "[ERROR in LevelADT remove!] Wrong kind!"))))
 
     (define (remove-from-inventory! kind)
-      (let ((keys (car inventory)))
+      (let ((keys-in-inventory (car inventory))
+            (bombs-in-inventory (cadr inventory)))
         (cond
-          ((eq? kind 'key) (set-car! inventory (cdr keys)))
+          ((eq? kind 'key) (set-car! inventory (cdr keys-in-inventory)))
+          ((eq? kind 'bomb) (set-car! (cdr inventory) (cdr bombs-in-inventory)))
           (else (error "[ERROR in LevelADT remove-from-inventory!]")))))
 
     (define (remove-and-add-to-inv! kind)
@@ -241,11 +270,13 @@
           ((((car lst) 'position) 'equal? (ant 'position)) (add-to-inventory! (car lst)) (cdr lst))
           (else (cons (car lst) (look-for-remove (cdr lst))))))
       (cond
-        ((eq? kind 'key) (set! keys (look-for-remove keys)))))
+        ((eq? kind 'key) (set! keys (look-for-remove keys)))
+        ((eq? kind 'bomb) (set! bombs (look-for-remove bombs)))))
 
     (define (add-to-inventory! object)
       (cond
         ((eq? (object 'kind) 'key) (set-car! inventory (cons 'key (car inventory))))
+        ((eq? (object 'kind) 'bomb) (set-car! (cdr inventory) (cons 'bomb (cadr inventory))))
         (else (error "[ERROR in LevelADT add-to-inventory!] Wrong kind!"))))
 
     ;;
@@ -264,6 +295,7 @@
       (cond
         ((not (can-move? ant direction 'egg)) (remove! 'egg) (update-score! (cons 500 (vector 0 0 0 0 0 5 0 0))))
         ((not (can-move? ant direction 'key)) (remove-and-add-to-inv! 'key))
+        ((not (can-move? ant direction 'bomb)) (remove-and-add-to-inv! 'bomb))
         ((not (can-move? ant direction 'shield-shroom)) (remove! 'shield-shroom) (set! shield? #t))
         ((not (can-move? ant direction 'food)) (remove! 'food) (lives! (+ lives 1)) (update-score! (cons 200 (vector 0 0 0 0 0 2 0 0))))))
 
@@ -300,12 +332,12 @@
         (define (move-random! scorpion)
           (scorpion 'new-orientation!)
           (let ((orientation (scorpion 'orientation)))
-            (if (and (can-move? scorpion orientation 'wall) (can-move? scorpion orientation 'door) (not (< ((next-position scorpion orientation) 'y) top-border)))
+            (if (and (can-move? scorpion orientation 'wall) (can-move? scorpion orientation 'door) (can-move? scorpion orientation 'weak-wall) (not (< ((next-position scorpion orientation) 'y) top-border)))
                 (scorpion 'move! distance)
                 (move-random! scorpion))))
         (define (move! scorpion)
           (let ((orientation (scorpion 'orientation)))
-            (if (and (can-move? scorpion orientation 'wall) (can-move? scorpion orientation 'door) (not (< ((next-position scorpion orientation) 'y) top-border)))
+            (if (and (can-move? scorpion orientation 'wall) (can-move? scorpion orientation 'door) (can-move? scorpion orientation 'weak-wall) (not (< ((next-position scorpion orientation) 'y) top-border)))
                 (begin
                   (scorpion 'move! distance)
                   (set! scorpion-time 0))
@@ -320,7 +352,7 @@
               (for-each move! random-scorpions)))))
 
     (define (move-ant! direction)
-      (if (and (can-move? ant direction 'wall) (can-move? ant direction 'door) (not (< ((next-position ant direction) 'y) top-border)))
+      (if (and (can-move? ant direction 'wall) (can-move? ant direction 'door) (can-move? ant direction 'weak-wall) (not (< ((next-position ant direction) 'y) top-border)))
           (begin
             (ant 'orientation! direction)
             (ant 'move! distance)
@@ -340,7 +372,9 @@
         ((eq? name 'random-scorpions) random-scorpions)
         ((eq? name 'eggs) eggs)
         ((eq? name 'keys) keys)
+        ((eq? name 'bombs) bombs)
         ((eq? name 'doors) doors)
+        ((eq? name 'weak-walls) weak-walls)
         ((eq? name 'shield-shrooms) shield-shrooms)
         ((eq? name 'food) food)
         (else (error "[ERROR in LevelADT give-list] Wrong name!"))))
@@ -353,7 +387,9 @@
       (cond
         ((eq? object-list 'egg) (member element eggs))
         ((eq? object-list 'key) (member element keys))
+        ((eq? object-list 'bomb) (member element bombs))
         ((eq? object-list 'door) (member element doors))
+        ((eq? object-list 'weak-wall) (member element weak-walls))
         ((eq? object-list 'shield-shroom) (member element shield-shrooms))
         ((eq? object-list 'food) (member element food))
         (else (error "[ERROR in LevelADT member?] Wrong object-list!"))))
@@ -363,6 +399,8 @@
         ((eq? object-list 'egg) (length eggs))
         ((eq? object-list 'key) (length keys))
         ((eq? object-list 'door) (length doors))
+        ((eq? object-list 'bomb) (length bombs))
+        ((eq? object-list 'weak-wall) (length weak-walls))
         ((eq? object-list 'shield-shroom) (length shield-shrooms))
         ((eq? object-list 'food) (length food))
         (else (error "[ERROR in LevelADT length?] Wrong object-list!"))))
@@ -376,7 +414,6 @@
         ((eq? message 'inventory) inventory)
         ((eq? message 'update-score?) update-score?)
         ((eq? message 'remove-live?) remove-live?)
-        ((eq? message 'speed-up?) speed-up?)
         ((eq? message 'shield?) shield?)
         ((eq? message 'give-list) (apply give-list parameters))
         ((eq? message 'done?) done?)
@@ -386,11 +423,11 @@
         ((eq? message 'add-eggs) (apply add-eggs parameters))
         ((eq? message 'add-puzzleobjects) (apply add-puzzleobjects parameters))
         ((eq? message 'add-powerups) (apply add-powerups parameters))
+        ((eq? message 'bomb-animation?) (apply bomb-animation parameters))
         ((eq? message 'lives) lives)
         ((eq? message 'lives!) (apply lives! parameters))
         ((eq? message 'remove-live!) (apply remove-live! parameters))
         ((eq? message 'speed-up) (apply speed-up parameters))
-        ((eq? message 'check-deactivate-speed-up!) (apply check-deactivate-speed-up! parameters))
         ((eq? message 'check-for-ant-scorpion-collision) (check-for-ant-scorpion-collision))
         ((eq? message 'check-deactivate-shield!) (apply check-deactivate-shield! parameters))
         ((eq? message 'move-ant!) (apply move-ant! parameters))
