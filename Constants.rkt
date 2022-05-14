f80f0b98acbdda5a6c6c0985549af79b6cd5621e
Vic De Hondt <vic.dehondt@gmail.com>
2022-05-14 15:49:07 +0200

    Better structure in constants file

--- Constants.rkt
+++ Constants.rkt
@@ -1,4 +1,15 @@
+;;
+;; WINDOW
+;;
+
+(define width 912)
+(define height 720)
+
 (define grid-cell 48)
+
+;;
+;; TIME
+;;
 
 (define normal-interval 600)
 (define speed-interval 300)
@@ -7,32 +18,58 @@
 
 (define shield-interval 15000)
 
-(define press-space-interval 500)
-(define press-space-splash-y 600)
-(define press-space-y 550)
-
 (define score-interval 1000)
+
+(define press-space-interval 500)
 
 (define bomb-animation-interval 5000)
 
 (define rumble-interval 200)
 
-(define distance 1)
+;;
+;; POSITION
+;;
 
-(define width 912)
-(define height 720)
+(define press-space-splash-y 600)
+(define press-space-y 550)
 
 (define score-size 8)
 (define score-x-spacing 31)
+
 (define score-number-x-start 372)
 (define score-number-y-start 624)
+
 (define highscore-number-x-start 372)
 (define highscore-number-y-start 656)
 
+(define score-text-x 168)
+(define score-text-y 624)
+
+(define highscore-text-x 0)
+(define highscore-text-y 656)
+
+(define lives-text-x 648)
+(define lives-text-y 624)
+
+(define lives-sequence-x 884)
+(define lives-sequence-y 624)
+
+(define sky-x 0)
+(define sky-y 0)
+
+(define distance 1)
+
+;;
+;; SCORES
+;;
+
 (define egg-score 500)
+
 (define food-score 200)
 
-(define lives-start-count 3)
+;;
+;; VECTOR INDEXES
+;;
 
 (define wall-place 0)
 (define scorpion-place 1)
@@ -43,20 +80,16 @@
 (define standard-index 0)
 (define speed-index 4)
 (define shield-index 8)
+
 (define up-index 0)
 (define right-index 1)
 (define down-index 2)
 (define left-index 3)
 
-(define score-text-x 168)
-(define score-text-y 624)
-(define highscore-text-x 0)
-(define highscore-text-y 656)
-(define lives-text-x 648)
-(define lives-text-y 624)
-(define lives-sequence-x 884)
-(define lives-sequence-y 624)
-(define sky-x 0)
-(define sky-y 0)
+;;
+;; LEVEL
+;;
+
+(define lives-start-count 3)
 
 (define first-level 1)
